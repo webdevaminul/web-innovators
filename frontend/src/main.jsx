77c7fcf68c564b2e3bdfc0e8be5cfcb80eb51e2c
Mohib <mohibullahmohim2020@gmail.com>
2024-09-20 01:45:01 +0600

    sign up page ready almost

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/Router";
+import { Slide, ToastContainer } from "react-toastify";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
+    <ToastContainer position="top-right" autoClose={5000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover theme="dark" transition={Slide}
+    />
   </StrictMode>
 );
