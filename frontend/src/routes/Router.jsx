cd9ad915639b1b89efdead18285dfdab48fa4daf
AbujafarCh <357jafar@gmail.com>
2024-09-20 16:48:53 +0600

    navbar, registe page and banner section edited

--- frontend/src/routes/Router.jsx
+++ frontend/src/routes/Router.jsx
@@ -13,7 +13,7 @@ const router = createBrowserRouter([
         element: <Home />,
       },
       {
-        path: "/sign-up",
+        path: "/register",
         element: <SignUp />,
       },
     ],
