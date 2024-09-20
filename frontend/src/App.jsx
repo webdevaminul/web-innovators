f49fd4c62d459de03d829549062b59fe3d2723b5
AbujafarCh <357jafar@gmail.com>
2024-09-20 11:32:46 +0600

    navbar added to project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "./Section/Navbar/Navbar";
 
 function App() {
   return (
     <main>
+      <Navbar />
       <Outlet />
     </main>
   );
