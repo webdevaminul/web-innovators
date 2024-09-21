96b8eddd0bdea961c535be3b5b7affdce5dce1e7
Md Fardin Jahan Showrav <mohammadshowrav15@gmail.com>
2024-09-21 20:35:21 +0600

    latest section added

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from "../../components/Banner/Banner";
+import LatestSection from "../../components/LatestSection/LatestSection";
 import Reviews from "../../components/Reviews/Reviews";
 
 export default function Home() {
   return (
     <section>
       <Banner />
+      <LatestSection />
       <Reviews />
     </section>
   );
