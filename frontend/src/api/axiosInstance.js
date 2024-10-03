7b3274cc4459efb7578f030ff9eb4cc68d68100f
Mohib <mohibullahmohim2020@gmail.com>
2024-10-03 21:29:22 +0600

    data come from development

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create an instance of axios with base configuration
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: `${import.meta.env.VITE_baseUrl}`,
   withCredentials: true,
 });
 
