e806ae875be2da403eef410d7144d6af2503db98
Mohib <mohibullahmohim2020@gmail.com>
2024-10-03 19:20:55 +0600

    form added in create course

--- frontend/src/pages/TeacherDashboard/CreateCourse.jsx
+++ frontend/src/pages/TeacherDashboard/CreateCourse.jsx
@@ -1,7 +1,59 @@
 const CreateCourse = () => {
   return (
-    <div>
-      <h1>Create a course </h1>
+    <div className="mx-14 mt-10 border-2 border-blue-400 rounded-lg">
+      <div className="mt-10 text-center font-bold">Upload Course</div>
+      <div className="mt-3 text-center text-4xl font-bold">
+        Share your experiene for student
+      </div>
+      <div className="p-8">
+        <div className="flex gap-4">
+          <input
+            type="Name"
+            name="name"
+            className="mt-1 block w-1/2 rounded-md border border-slate-300 bg-white px-3 py-4 placeholder-slate-400 shadow-sm placeholder:font-semibold placeholder:text-gray-500 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm"
+            placeholder="Full Name *"
+          />
+          <input
+            type="email"
+            name="email"
+            className="mt-1 block w-1/2 rounded-md border border-slate-300 bg-white px-3 py-4 placeholder-slate-400 shadow-sm placeholder:font-semibold placeholder:text-gray-500 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm"
+            placeholder="Email *"
+          />
+        </div>
+        <div className="my-6 flex gap-4">
+          <select
+            name="select"
+            id="select"
+            className="block w-1/2 rounded-md border border-slate-300 bg-white px-3 py-4 font-semibold text-gray-500 shadow-sm focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm"
+          >
+            <option className="font-semibold ">Please Select</option>
+            <option className="font-semibold">Freelancing</option>
+            <option className="font-semibold">Web Design</option>
+          </select>
+
+          <label className="form-control w-full max-w-xs">
+            <input
+              type="file"
+              className="file-input file-input-bordered w-full max-w-xs"
+            />
+          </label>
+        </div>
+        <div>
+          <textarea
+            name="textarea"
+            id="text"
+            cols={30}
+            rows={10}
+            className="mb-10 h-40 w-full resize-none rounded-md border border-slate-300 p-5 font-semibold text-gray-300"
+            defaultValue={"Message"}
+          />
+        </div>
+        <div className="text-center">
+          <a className="cursor-pointer rounded-lg bg-blue-700 px-8 py-5 text-sm font-semibold text-white">
+            Submit
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
