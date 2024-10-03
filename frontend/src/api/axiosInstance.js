9c659bbecbfd890808de36a96e012d7071e32118
Mohib <mohibullahmohim2020@gmail.com>
2024-10-04 00:28:16 +0600

    All page created just

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create an instance of axios with base configuration
 const axiosInstance = axios.create({
-  baseURL: `${import.meta.env.VITE_baseUrl}`,
+  baseURL: "http://localhost:5000",
   withCredentials: true,
 });
 
