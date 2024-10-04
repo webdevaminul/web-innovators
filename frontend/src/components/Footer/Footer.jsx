daad0abae84dfda386e1a0aa295ea1437fa6179e
Md Fardin Jahan Showrav <mohammadshowrav15@gmail.com>
2024-10-04 14:21:27 +0600

    pagination added

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -8,11 +8,7 @@ import { FaWhatsapp } from "react-icons/fa";
 
 const Footer = () => {
   return (
-<<<<<<< HEAD
-    <footer className="bg-accentOne mt-8">
-=======
     <footer className="bg-accentOne py-4">
->>>>>>> 13ca6cddce396fcba598ae35ca85c66d74272a13
       <div className="container mx-auto font-bai gap-5 md:py-10 grid grid-cols-2 md:grid-cols-4">
         {/* Innovators Name */}
         <div className="md:mt-10 col-span-2 md:col-span-1 text-center md:text-left">
