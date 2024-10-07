867417502985044b56eaf6e87d178e5d13240931
Md Fardin Jahan Showrav <mohammadshowrav15@gmail.com>
2024-10-07 11:24:09 +0600

    course details route created

--- frontend/src/pages/AllCourses/CourseCard.jsx
+++ frontend/src/pages/AllCourses/CourseCard.jsx
@@ -1,30 +1,32 @@
 import React from "react";
 import { FaBangladeshiTakaSign } from "react-icons/fa6";
+import { Link } from "react-router-dom";
 
 const CourseCard = ({ course }) => {
   return (
-    <>
-      <div className="card card-compact border border-border bg-accentOne">
-        <figure>
-          <img src={course.image} alt="Shoes" />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">{course.title}</h2>
-          <p>{course.instructor}</p>
+    <Link
+      to={`/course-details/${course.id}`}
+      className="card card-compact border border-border bg-accentOne"
+    >
+      <figure>
+        <img src={course.image} alt="Shoes" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{course.title}</h2>
+        <p>{course.instructor}</p>
 
-          <div>
-            <p className="flex justify-start items-center text-xl text-secondary">
+        <div>
+          <p className="flex justify-start items-center text-xl text-secondary">
+            <FaBangladeshiTakaSign />
+            {course.price}
+            <span className="line-through text-text flex justify-start items-center ml-2 text-base">
               <FaBangladeshiTakaSign />
-              {course.price}
-              <span className="line-through text-text flex justify-start items-center ml-2 text-base">
-                <FaBangladeshiTakaSign />
-                {course.old_price}
-              </span>
-            </p>
-          </div>
+              {course.old_price}
+            </span>
+          </p>
         </div>
       </div>
-    </>
+    </Link>
   );
 };
 
---
+++ frontend/src/pages/AllCourses/CourseDetails.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { useLoaderData, useParams } from "react-router-dom";
+
+const CourseDetails = () => {
+  const coursesDetails = useLoaderData();
+
+  const { id } = useParams();
+  console.log(id);
+  const courseId = parseInt(id);
+  const singleCourse = coursesDetails.find((data) => data.id === courseId);
+  console.log(singleCourse);
+  return (
+    <div>
+      <h1>Course Details {singleCourse.id}</h1>
+    </div>
+  );
+};
+
+export default CourseDetails;
