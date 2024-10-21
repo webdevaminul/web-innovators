7b59062aacf20588f5f2785b6cda3adc3f86c9c3
webdevaminul <webdev.aminul@gmail.com>
2024-10-22 00:04:33 +0600

    remove query for search user

--- backend/api/controllers/course.controller.js
+++ backend/api/controllers/course.controller.js
@@ -32,14 +32,14 @@ exports.createCourse = async (req, res, next) => {
     const result = await courseCollection.insertOne(newCourse);
 
     // Success response
-    res.status(201).json({
+    return res.status(201).json({
       message: "Course created successfully!",
       courseId: result.insertedId,
       course: newCourse,
     });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "Internal server error" });
+    return res.status(500).json({ message: "Internal server error" });
     next(error);
   }
 };
@@ -80,7 +80,7 @@ exports.availableCourse = async (req, res, next) => {
     }
 
     // Send response if courses are found
-    res.status(200).json({
+    return res.status(200).json({
       success: true,
       data: courses,
       currentPage,
@@ -88,7 +88,7 @@ exports.availableCourse = async (req, res, next) => {
       totalCourses, // Total number of courses for client to know
     });
   } catch (error) {
-    res.status(500).json({
+    return res.status(500).json({
       success: false,
       message: "An error occurred while fetching courses. Please try again later.",
     });
@@ -109,12 +109,12 @@ exports.allCourse = async (req, res, next) => {
     }
 
     // Always return a valid response with an array
-    res.status(200).json({
+    return res.status(200).json({
       success: true,
       data: courses,
     });
   } catch (error) {
-    res.status(500).json({
+    return res.status(500).json({
       success: false,
       message: "An error occurred while fetching courses. Please try again later.",
     });
@@ -134,12 +134,12 @@ exports.updateCourse = async (req, res, next) => {
       },
     };
     const result = await courseCollection.updateOne(query, updateDoc);
-    res.status(200).json({
+    return res.status(200).json({
       message: `This course has been ${updateStatus} successfully!`,
       data: result,
     });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    return res.status(500).json({ message: error.message });
     next(error);
   }
 };
